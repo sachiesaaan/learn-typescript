1527987b5319bd1d10e7916874426f2ababfaca6
standingflower0607 <tachibanaa1211@gmail.com>
2025-01-27 18:45:28 +0900

    finish like-button

---
+++ like-button/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import "./App.css"
+
+function App() {
+  return (
+    <>
+     <LikeButton/> 
+    </>
+  )
+}
+
+function LikeButton(){
+  const [count, setCount] = useState(999);
+  const handleClick = () =>{
+    setCount(count + 1);
+  }
+  return <span className="likeButton" onClick={handleClick}>♥ {count}</span>
+}
+export default App
